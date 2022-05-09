7c7eb70b7e6a57b49b0a7067cbecc97ef8c1fe47
hugosenari <hugosenari@gmail.com>
2022-05-09 20:35:05 -0300

    chore: re index aossoa

--- flake.nix
+++ flake.nix
@@ -7,22 +7,6 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs."aossoa-master".dir   = "master";
-  inputs."aossoa-master".owner = "nim-nix-pkgs";
-  inputs."aossoa-master".ref   = "master";
-  inputs."aossoa-master".repo  = "aossoa";
-  inputs."aossoa-master".type  = "github";
-  inputs."aossoa-master".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."aossoa-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
-  inputs."aossoa-v0_1_0".dir   = "v0_1_0";
-  inputs."aossoa-v0_1_0".owner = "nim-nix-pkgs";
-  inputs."aossoa-v0_1_0".ref   = "master";
-  inputs."aossoa-v0_1_0".repo  = "aossoa";
-  inputs."aossoa-v0_1_0".type  = "github";
-  inputs."aossoa-v0_1_0".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."aossoa-v0_1_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
